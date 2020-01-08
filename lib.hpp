edacb79f5a7bbd61bcf7310fde296bb007085e04
GitProger <51924999+GitProger@users.noreply.github.com>
2020-01-09 00:33:13 +0300

    Update lib.hpp

--- lib.hpp
+++ lib.hpp
@@ -62,9 +62,9 @@ string a;while(x--)a+=s;return a;}ll factorial(int x){ll a=1;for(int i=1;i<=x;i
 ++)a*=i;return a;}template<class T>void reverse(vector<T>&a){for(int i=0;i<(int)
 a.size()/2;i++)swap(a[i],a[a.size()-1-i]);}
 
-template<class T>struct reader:public T{template<class...Con>reader(Con...par):T
-(par...){cin>>*this;}};template<>class reader<int>{int x;public:reader(){cin>>x;
-}operator int(){return x;}};
+template<class T>struct reader:public T{template<class...Con>reader(Con...&&par)
+:T(par...){cin>>*this;}};template<>class reader<int>{int x;public:reader(){cin>>
+x;}operator int(){return x;}};
 
 class TaskAnswer{};void answer(){cout<<$;throw TaskAnswer();}template<class C,
 class...Ts>void answer(C&&cur,Ts&&...args){cout<<cur;answer(args...);}class
