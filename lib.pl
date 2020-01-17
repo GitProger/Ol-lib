4169b7793ad4abafbecbc4537fb12a92dcb3a014
GitProger <51924999+GitProger@users.noreply.github.com>
2020-01-17 10:33:07 +0300

    Create lib.pl

---
+++ lib.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+package Codeforces::Task::_;
+use v5.10;
+use strict;
+use warnings;
+
+sub min{my($a,$b)=@_;if($a<$b){return$a;}else{return$b;}}
+sub max{my($a,$b)=@_;if($a>$b){return$a;}else{return$b;}}
+
+sub main {
+    my $t = 1;
+#    $t = int(<>);
+    while ($t--) {
+        solve();
+    }
+    return 0;
+}
+
+exit(main(@ARGV));
