decd62b061a89918c447b2acf2c1532af1270d8a
GitProger <51924999+GitProger@users.noreply.github.com>
2020-01-27 18:22:27 +0300

    Update segment_tree.hpp

--- segment_tree.hpp
+++ segment_tree.hpp
@@ -1,9 +1,11 @@
-#define BUF 100
+#ifndef BUF
+#define BUF 2048
+#endif
 
 class segment_tree {
     private:
-        int *t;
-        int n;
+        int *t, n;
+    
         void build(const veci &a, int v = 1, int tl = 0, int tr = n - 1) {
             if (tl == tr) {
                 t[v] = a[tl];
@@ -14,21 +16,23 @@ class segment_tree {
             build(a, v * 2 + 1, tm + 1, tr);
             t[v] = t[v * 2] + t[v * 2 + 1];
         }
+    
     public:
         segment_tree(const vector<int> &a) {
             this->n = a.size();
             this->t = new int[this->n + BUF];
             build(a);
         }
+    
         int sum(int v = 1, int tl = 0, int tr = n - 1, int l, int r) {
             if (l > r)
                 return 0;
             if (l == tl && r == tr)
                 return t[v];
             int tm = (tl + tr) / 2;
-           return sum(v * 2, tl, tm, l, min(r, tm))
-                + sum(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r);
+            return sum(v * 2, tl, tm, l, min(r, tm))  +  sum(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r);
         }
+    
         void assign(int v = 1, int tl = 0, int tr = n - 1, int pos, int new_val) {
             if (tl == tr) {
                 t[v] = new_val;
