9e6a8ea8ad691d03061dda850ee7bc3d138df4e0
GitProger <51924999+GitProger@users.noreply.github.com>
2020-02-04 12:13:50 +0300

    Update lib.kt

--- lib.kt
+++ lib.kt
@@ -1,4 +1,7 @@
-private fun solve() {
+import java.math.*
+import kotlin.math.*
+
+fun solve() {
 
 }
 
@@ -18,4 +21,29 @@ class yesno(y: String, n: String) {
     private val yes = y
     private val no = n
     operator fun invoke(ok: Boolean): String { return if (ok) this.yes else this.no }
+}
+
+
+fun prefixFunction(s: String): Array<Int> {
+    val n = s.length
+    val p = Array(n) {0}
+    for (i in 1 until n) {
+        var j = p[i - 1]
+        while (j > 0 && s[j] != s[i])
+            j = p[j - 1]
+        if (s[i] == s[j])
+            j++
+        p[i] = j
+    }
+    return p
+}
+
+fun KnuthMorris(s: String, t: String): Int {
+    val p = prefixFunction("$s#$t")
+    val n = s.length
+    val m = t.length
+    for (i in n + 1 .. n + m)
+        if (p[i] == n)
+            return i - n - n
+    return -1
 }
