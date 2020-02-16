540202470921fa6b7739093803791a9e05cb4bb0
GitProger <51924999+GitProger@users.noreply.github.com>
2020-02-16 23:47:27 +0300

    Update lib.pl

--- lib.pl
+++ lib.pl
@@ -4,8 +4,13 @@ use v5.10;
 use strict;
 use warnings;
 
+
 sub min{my($a,$b)=@_;if($a<$b){return$a;}else{return$b;}}
 sub max{my($a,$b)=@_;if($a>$b){return$a;}else{return$b;}}
+
+sub solve {
+
+}
 
 sub main {
     my $t = 1;
